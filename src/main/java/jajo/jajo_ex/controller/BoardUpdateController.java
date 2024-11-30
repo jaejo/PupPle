01edd68685c0e75accf80f73428e941aa1582e05
DESKTOP-0JVG8OV\LEEJAEJOON <ljjn0520@gmail.com>
2024-11-30 15:37:53 +0900

    게시글 삭제 시 이미지 파일 동반 삭제 수정

--- src/main/java/jajo/jajo_ex/controller/BoardUpdateController.java
+++ src/main/java/jajo/jajo_ex/controller/BoardUpdateController.java
@@ -1,9 +1,12 @@
 package jajo.jajo_ex.controller;
 
+import jajo.jajo_ex.BoardType;
 import jajo.jajo_ex.domain.Board;
+import jajo.jajo_ex.domain.BoardFile;
 import jajo.jajo_ex.domain.Member;
 import jajo.jajo_ex.dto.BoardRequestDto;
 import jajo.jajo_ex.dto.ResponseDto;
+import jajo.jajo_ex.service.BoardFileService;
 import jajo.jajo_ex.service.BoardService;
 import jajo.jajo_ex.service.MemberService;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,16 +16,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.util.List;
+
 @RestController
 @RequestMapping("/board")
 public class BoardUpdateController {
     private final MemberService memberService;
     private final BoardService boardService;
+    private final BoardFileService boardFileService;
 
     @Autowired
-    public BoardUpdateController(MemberService memberService, BoardService boardService) {
+    public BoardUpdateController(MemberService memberService, BoardService boardService, BoardFileService boardFileService) {
         this.memberService = memberService;
         this.boardService = boardService;
+        this.boardFileService = boardFileService;
     }
 
 //    @DeleteMapping("/deleteForm")
@@ -30,7 +38,22 @@ public class BoardUpdateController {
 //        //boardService.deleteBoard(no);
 //    }
     @DeleteMapping("/deleteForm")
-    public ResponseDto<Integer> delete1(@RequestParam("no") Long no){
+    public ResponseDto<Integer> deleteOne(@RequestParam("no") Long no, @RequestParam("boardType") BoardType boardType){
+
+        List<BoardFile> boardFile = boardFileService.findByBoard(no);
+
+        String removeFile = boardFile.get(0).getFileName();
+        for (String removeFileName : removeFile.split(",")) {
+            removeFileName = removeFileName.trim();
+            if (removeFileName != "") {
+                String DeleteFileDir = "C:\\Users\\LEEJAEJOON\\Documents\\PupPle\\src\\main\\resources\\static\\uploadImage\\";
+                DeleteFileDir += boardType + "\\" + removeFileName;
+                File DeleteFile = new File(DeleteFileDir);
+                if (DeleteFile.delete()) {
+                    System.out.println(removeFileName + ": 삭제 완료");
+                }
+            }
+        }
         boardService.deleteBoard(no);
         return ResponseDto.success(1);
     }
